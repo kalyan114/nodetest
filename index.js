da59db7abebfb2061d21ef511fd1f5bdec1549b8
kalyankumarthanguturi333 <kalyankumarthanguturi333@gmail.com>
2024-06-10 10:09:53 +0000

    new commit 10 jun

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express();
 
-const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
+const port = parseInt(process.env.PORT) || process.argv[3] || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -11,10 +11,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.render('index');
+  // res.render('index.js');
 });
 
 app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
+  // res.json({"msg": "Hello world"});
   res.json({"msg": "hi this is my new project"});
 });
 
